df90eadbe4e00e605c18d423de897c8ddf7abe11
Maria Lacayo <maria@dwim.mx>
2023-04-17 10:29:53 -0600

    Scanner code in Main Activity

--- app/src/main/java/mx/lacayo/maria/qrcodescanner/MainActivity.java
+++ app/src/main/java/mx/lacayo/maria/qrcodescanner/MainActivity.java
@@ -1,14 +1,59 @@
 package mx.lacayo.maria.qrcodescanner;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button scan_btn;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scan_btn = findViewById(R.id.scanner);
+        textView = findViewById(R.id.text);
+
+        scan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setPrompt("Scan a QR Code");
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                intentIntegrator.initiateScan();
+            }
+        });
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode, data);
+        if(intentResult != null){
+            String contents = intentResult.getContents();
+            if(contents != null){
+                textView.setText(intentResult.getContents());
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+    }
+    
+
 }
